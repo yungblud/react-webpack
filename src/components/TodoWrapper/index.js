c9f240a85e6b6af52cf7e9df71f64f0a8a1dd5b0
Dongho Choi <kkiathorn@gmail.com>
2019-12-23 12:45:32 +0900

    modified markdown

--- src/components/TodoWrapper/index.js
+++ src/components/TodoWrapper/index.js
@@ -4,25 +4,6 @@ import s from './style.scss'
 import TodoTitle from '../TodoTitle'
 import TodoList from '../TodoList'
 
-// const todoList = [
-//     {
-//         id: 0,
-//         todo: 'walking around on a sunny day',
-//     },
-//     {
-//         id: 1,
-//         todo: 'take vitamin pills',
-//     },
-//     {
-//         id: 2,
-//         todo: 'watch netflix',
-//     },
-//     {
-//         id: 3,
-//         todo: 'eat ice cream',
-//     },
-// ]
-
 const TodoWrapper = () => {
     const [todoList, setTodoList] = useState([])
 
