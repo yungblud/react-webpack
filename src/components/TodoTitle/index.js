683dc7c17697a092e8caf3647f8d2886407d713a
Dongho Choi <kkiathorn@gmail.com>
2019-12-23 11:32:08 +0900

    2.todolist

--- src/components/TodoTitle/index.js
+++ src/components/TodoTitle/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import s from './style.scss'
 
 const TodoTitle = () => {
-    return <div className={s.wrapper}>TodoTitle</div>
+    return <div className={s.wrapper}>React Todo List</div>
 }
 
 export default TodoTitle
--- src/components/TodoWrapper/index.js
+++ src/components/TodoWrapper/index.js
@@ -3,11 +3,30 @@ import s from './style.scss'
 import TodoTitle from '../TodoTitle'
 import TodoList from '../TodoList'
 
+const todoList = [
+    {
+        id: 0,
+        todo: 'walking around on a sunny day',
+    },
+    {
+        id: 1,
+        todo: 'take vitamin pills',
+    },
+    {
+        id: 2,
+        todo: 'watch netflix',
+    },
+    {
+        id: 3,
+        todo: 'eat ice cream',
+    },
+]
+
 const TodoWrapper = () => {
     return (
         <div className={s.wrapper}>
             <TodoTitle />
-            <TodoList />
+            <TodoList todoList={todoList} />
         </div>
     )
 }
